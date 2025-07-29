03817af119d58ae1715ab7c952da6bcf1f8041d9
Pedro Henrique Nogueira <nogueirapedro404@gmail.com>
2025-07-29 15:21:54 -0500

    Untracked ignored files and re-added others

--- src/bhowmik2025plots/input_files/utils/arc_to_au.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def arc_to_au(distance_pc):
-    """
-    Purpose: Small function to convert from arcsec to au given distance in parsecs
-    """
-    au = (np.pi / (180 * 3600)) * distance_pc * 206265
-    return au
